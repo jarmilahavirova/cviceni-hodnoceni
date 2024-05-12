0fc5a7f3a96acbe43b901a3bcdf6c8e0e987233f
Jarmila Havířová <161177140+jarmilahavirova@users.noreply.github.com>
2024-05-12 22:22:48 +0200

    Vyřešena druhá část úkolu, zprovoznění všech hvězdiček

--- src/components/Rating/index.jsx
+++ src/components/Rating/index.jsx
@@ -3,17 +3,17 @@ import { Star } from "../Star";
 import "./style.css";
 
 export const Rating = () => {
-  const [value, setValue] = useState(0);
+  const [rating, setRating] = useState(0);
 
   return (
     <div className="rating">
-      <div className="rating__value">{value}</div>
+      <div className="rating__value">{rating}</div>
       <div className="rating__stars">
-        <Star value={1} onSelect={setValue} />
-        <Star value={2} onSelect={setValue} />
-        <Star value={3} onSelect={setValue} />
-        <Star value={4} onSelect={setValue} />
-        <Star value={5} onSelect={setValue} />
+        <Star glowing={rating >= 1} value={1} onSelect={setRating} />
+        <Star glowing={rating >= 2} value={2} onSelect={setRating} />
+        <Star glowing={rating >= 3} value={3} onSelect={setRating} />
+        <Star glowing={rating >= 4} value={4} onSelect={setRating} />
+        <Star glowing={rating >= 5} value={5} onSelect={setRating} />
       </div>
     </div>
   );
--- src/components/Star/index.jsx
+++ src/components/Star/index.jsx
@@ -1,16 +1,13 @@
 import { useState } from "react";
 import "./style.css";
 
-export const Star = ({ value, onSelect }) => {
-  const [glowing, setGlowing] = useState(false);
-
+export const Star = ({ glowing, value, onSelect }) => {
   const starClass = glowing ? "rating__star rating__star--on" : "rating__star";
 
   return (
     <div
       className={starClass}
       onClick={() => {
-        setGlowing(true);
         onSelect(value);
       }}
     ></div>
