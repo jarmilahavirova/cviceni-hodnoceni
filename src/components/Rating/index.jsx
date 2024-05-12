37eee3edc2810b3b4abb5ef5dcdff156f6685cfa
Jarmila Havířová <161177140+jarmilahavirova@users.noreply.github.com>
2024-05-12 21:57:52 +0200

    Vyřešení první části úkolu

--- src/components/Rating/index.jsx
+++ src/components/Rating/index.jsx
@@ -1,19 +1,19 @@
-import { useState } from 'react';
-import { Star } from '../Star';
-import './style.css';
+import { useState } from "react";
+import { Star } from "../Star";
+import "./style.css";
 
 export const Rating = () => {
   const [value, setValue] = useState(0);
-  
+
   return (
     <div className="rating">
       <div className="rating__value">{value}</div>
       <div className="rating__stars">
-        <Star />
-        <Star />
-        <Star />
-        <Star />
-        <Star />
+        <Star value={1} onSelect={setValue} />
+        <Star value={2} onSelect={setValue} />
+        <Star value={3} onSelect={setValue} />
+        <Star value={4} onSelect={setValue} />
+        <Star value={5} onSelect={setValue} />
       </div>
     </div>
   );
--- src/components/Star/index.jsx
+++ src/components/Star/index.jsx
@@ -1,14 +1,18 @@
-import { useState } from 'react';
-import './style.css';
+import { useState } from "react";
+import "./style.css";
 
-export const Star = () => {
+export const Star = ({ value, onSelect }) => {
   const [glowing, setGlowing] = useState(false);
-  
-  const starClass = glowing
-    ? 'rating__star rating__star--on'
-    : 'rating__star';
+
+  const starClass = glowing ? "rating__star rating__star--on" : "rating__star";
 
   return (
-    <div className={starClass}></div>
+    <div
+      className={starClass}
+      onClick={() => {
+        setGlowing(true);
+        onSelect(value);
+      }}
+    ></div>
   );
 };
